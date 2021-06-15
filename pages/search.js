f4fa100216a5c54b61203caa0f3c036608828e6c
rarespurtan <purtan.rares@yahoo.com>
2021-06-15 21:16:45 +0100

    chore: finish search page

--- pages/search.js
+++ pages/search.js
@@ -1,17 +1,43 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "../components/Header";
+import Response from "../response";
+import SearchResults from "../components/SearchResults";
+import Footer from "../components/Footer";
 
-function Search() {
+function Search({ results }) {
+  const router = useRouter();
   return (
     <div>
       <Head>
-        <title>Search Results</title>
+        <title>{router.query.term} - Google Search</title>
       </Head>
-      {/* Header */}
       <Header />
-      {/* Search Results*/}
+      <SearchResults results={results} />
+      <Footer />
     </div>
   );
 }
 
 export default Search;
+
+export async function getServerSideProps(context) {
+  const useDummyData = true;
+  const startIndex = context.query.start || "0";
+
+  const data = useDummyData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+      )
+        .then((response) => response.json())
+        .catch((err) => {
+          console.error(err);
+        });
+  // once rendered, pass results to client
+  return {
+    props: {
+      results: data,
+    },
+  };
+}
