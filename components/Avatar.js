769e3434e7cee432489ab8781896f0b1ae56657f
rarespurtan <purtan.rares@yahoo.com>
2021-06-15 18:11:03 +0100

    feat: add avatar on search page

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,9 +1,9 @@
-function Avatar({ url }) {
+function Avatar({ url, classAttr }) {
   return (
     <div>
       <img
         loading='lazy'
-        className='h-10 w-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110'
+        className={`h-10 w-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${classAttr}`}
         src={url}
         alt='profile picture'
       />
--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Avatar from "../components/Avatar";
 import { useRouter } from "next/router";
 import { useRef } from "react";
 import { MicrophoneIcon, SearchIcon, XIcon } from "@heroicons/react/solid";
@@ -7,34 +8,51 @@ function Header() {
   const router = useRouter();
   const searchInputRef = useRef(null);
 
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+    if (!term) return;
+    router.push(`/search?term=${term}`);
+  };
+
   return (
-    <header>
-      <Image
-        src={
-          "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
-        }
-        height={40}
-        width={120}
-        className='cursor-pointer'
-        onClick={() => {
-          router.push("/");
-        }}
-      />
-      <form className='flex px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-xl max-w-3xl items-center'>
-        <input
-          ref={searchInputRef}
-          input='text'
-          className='flex-grow w-full focus:outline-none'
-        />
-        <XIcon
-          className='h-7 text-gray-500 sm:mr-3 cursor-pointer transition duration-100 transform hover:scale-125'
+    <header className='sticky top-0 bg-white'>
+      <div className='flex w-full p-6 items-center'>
+        <Image
+          src={
+            "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+          }
+          height={40}
+          width={120}
+          className='cursor-pointer'
           onClick={() => {
-            searchInputRef.current.value = "";
+            router.push("/");
           }}
         />
-        <MicrophoneIcon className='h-6 mr-3 hidden sm:inline-flex text-blue-500 border-l-2 pl-3 border-gray-300' />
-        <SearchIcon className='h-6 text-blue-500 hidden sm:inline-flex' />
-      </form>
+        <form className='flex flex-grow px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-xl max-w-3xl items-center'>
+          <input
+            ref={searchInputRef}
+            input='text'
+            className='flex-grow w-full focus:outline-none'
+          />
+          <XIcon
+            className='h-7 text-gray-500 sm:mr-3 cursor-pointer transition duration-100 transform hover:scale-125'
+            onClick={() => {
+              searchInputRef.current.value = "";
+            }}
+          />
+          <MicrophoneIcon className='h-6 mr-3 hidden sm:inline-flex text-blue-500 border-l-2 pl-3 border-gray-300' />
+          <SearchIcon className='h-6 text-blue-500 hidden sm:inline-flex' />
+          <button hidden type='submit' onClick={search}>
+            Search
+          </button>
+        </form>
+        <Avatar
+          // TODO: margin-left: auto not working on search page
+          classAttr='ml-auto'
+          url='https://images.unsplash.com/photo-1623265041640-8973a7afe898?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80'
+        />
+      </div>
     </header>
   );
 }
